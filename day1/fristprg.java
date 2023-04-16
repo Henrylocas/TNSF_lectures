b5a76e80a4eef041ae0d0e8836a2ff23d381830d
Henrylocas <129830144+Henrylocas@users.noreply.github.com>
2023-04-16 19:53:14 +0530

    Create fristprg.java

---
+++ day1/fristprg.java
@@ -0,0 +1,11 @@
+package Day1;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		System.out.println("I am in M28");
+		System.out.println("Hello to the M28 batch");
+
+	}
+
+}
