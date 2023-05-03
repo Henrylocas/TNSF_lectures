e2cb39b0c08ddf570d129a1048a57b56e308f084
RahulYadav <129830144+Henrylocas@users.noreply.github.com>
2023-05-03 20:27:43 +0530

    Create NestedTry.java

---
+++ day11/NestedTry.java
@@ -0,0 +1,25 @@
+class NestedTry {
+
+	public static void main(String args[])
+	{
+		try {
+
+			int a[] = { 1, 2, 3, 4, 5 };
+
+			System.out.println(a[5]);
+
+			// try-block2 inside another try block
+			try {
+
+				int x = a[2] / 0;
+			}
+			catch (ArithmeticException e2) {
+				System.out.println("division by zero is not possible");
+			}
+		}
+		catch (ArrayIndexOutOfBoundsException e1) {
+			System.out.println("ArrayIndexOutOfBoundsException");
+			System.out.println("Element at such index does not exists");
+		}
+	}
+}
