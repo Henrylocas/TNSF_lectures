c46153441e2024004d567abd5afe241ac5b62f03
RahulYadav <129830144+Henrylocas@users.noreply.github.com>
2023-05-03 19:42:09 +0530

    Create A.java

---
+++ day4/A.java
@@ -0,0 +1,14 @@
+public class A
+{ 
+   public void display() 
+      { 
+          System.out.println("SoftwareTestingHelp!!"); 
+      } 
+} 
+
+    public static void main(String args[]) 
+      { 
+          A obj = new A (); 
+          obj.display(); 
+      } 
+}
