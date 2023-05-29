97bbbf5f99ef3ce82e585c43cbfd030a80673564
RahulYadav <129830144+Henrylocas@users.noreply.github.com>
2023-05-29 12:53:42 +0530

    Create isodd.java

---
+++ day19/isodd.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class isodd {
+
+	public static void main(String[] args) {
+		Integer[] tihNumbers = {11,12,22,15,86,55,33,64};
+		List<Integer> numberList = Arrays.asList(tihNumbers);
+
+		numberList.stream().filter(n -> n%2!=0).forEach(System.out::println);
+	}
+
+}
